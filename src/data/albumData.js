678239d9bef5b553ec7de17a8a3861812742d6d7
Randy Cullins <randycullins2018@gmail.com>
2022-12-29 15:57:51 -0700

    portfolio page started

---
+++ src/data/albumData.js
@@ -0,0 +1,156 @@
+export const albumData = [
+  {
+    id: 0,
+    bandName: "Chairea",
+    player: (
+      <iframe
+        title="Chairea"
+        style={{ border: "0", width: "350px", height: "470px" }}
+        src="https://bandcamp.com/EmbeddedPlayer/album=1209895196/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/track=4165751693/transparent=true/"
+      />
+    ),
+  },
+  {
+    id: 1,
+    bandName: "Chairea",
+    player: (
+      <iframe
+        title="Chairea"
+        style={{ border: "0", width: "350px", height: "470px" }}
+        src="https://open.spotify.com/embed/album/5feTwq8XdUk32Cqs1YTdll?utm_source=generator"
+      />
+    ),
+  },
+  {
+    id: 2,
+    bandName: "Treasure State",
+    player: (
+      <iframe
+        title="Treasure State"
+        style={{ border: "0", width: "350px", height: "470px" }}
+        src="https://open.spotify.com/embed/artist/14uhuj5LTdNyF0SjDQ0N2p?utm_source=generator"
+      />
+    ),
+  },
+  {
+    id: 3,
+    bandName: "Anaconda Vise",
+    player: (
+      <iframe
+        title="Anaconda Vise"
+        style={{ border: "0", width: "350px", height: "470px" }}
+        src="https://open.spotify.com/embed/album/3TBZVwGQJQBvF2R72R0aux?utm_source=generator"
+      />
+    ),
+  },
+  {
+    id: 4,
+    bandName: "Anaconda Vise",
+    player: (
+      <iframe
+        title="Anaconda Vise"
+        style={{ border: "0", width: "350px", height: "470px" }}
+        src="https://open.spotify.com/embed/album/2079whpdnN9I0QFEIDevEA?utm_source=generator"
+      />
+    ),
+  },
+  {
+    id: 5,
+    bandName: "NVM",
+    player: (
+      <iframe
+        title="NVM"
+        style={{ border: "0", width: "350px", height: "470px" }}
+        src="https://open.spotify.com/embed/track/0PH22JbOIKUc10QRur2dfF?utm_source=generator"
+      />
+    ),
+  },
+  {
+    id: 6,
+    bandName: "NVM",
+    player: (
+      <iframe
+        title="NVM"
+        style={{ border: "0", width: "350px", height: "470px" }}
+        src="https://open.spotify.com/embed/album/1TENIZOVFTerspCdF6WSrB?utm_source=generator"
+      />
+    ),
+  },
+  {
+    id: 6,
+    bandName: "Gray Joy",
+    player: (
+      <iframe
+        title="Gray Joy"
+        style={{ border: "0", width: "350px", height: "470px" }}
+        src="https://open.spotify.com/embed/album/40fKk5JQEvLzvAm13mF6eg?utm_source=generator"
+      />
+    ),
+  },
+  {
+    id: 7,
+    bandName: "Dragged Out",
+    player: (
+      <iframe
+        title="Dragged Out"
+        style={{ border: "0", width: "350px", height: "470px" }}
+        src="https://open.spotify.com/embed/album/4bke4myL2rF0rQ38Tuc33w?utm_source=generator"
+      />
+    ),
+  },
+  {
+    id: 8,
+    bandName: "Sky Drftr",
+    player: (
+      <iframe
+        title="Sky Drftr"
+        style={{ border: "0", width: "350px", height: "470px" }}
+        src="https://open.spotify.com/embed/album/6W7WiVKzeSltRdJwT1x9WF?utm_source=generator"
+      />
+    ),
+  },
+  {
+    id: 9,
+    bandName: "Tom Russell",
+    player: (
+      <iframe
+        title="Tom Russell"
+        style={{ border: "0", width: "350px", height: "470px" }}
+        src="https://open.spotify.com/embed/album/6EOmUYkuN4lwsnyFLgW9rR?utm_source=generator"
+      />
+    ),
+  },
+  {
+    id: 10,
+    bandName: "Hubba Hubba",
+    player: (
+      <iframe
+        title="Hubba Hubba"
+        style={{ border: "0", width: "350px", height: "470px" }}
+        src="https://open.spotify.com/embed/album/2o7MgNvJ7k9u9La0V1kTsu?utm_source=generator"
+      />
+    ),
+  },
+  {
+    id: 11,
+    bandName: "Hubba Hubba",
+    player: (
+      <iframe
+        title="Hubba Hubba"
+        style={{ border: "0", width: "350px", height: "470px" }}
+        src="https://open.spotify.com/embed/album/3ugj0BjKZt2TzG2t7hC8Z9?utm_source=generator"
+      />
+    ),
+  },
+  {
+    id: 12,
+    bandName: "Tiny Iota",
+    player: (
+      <iframe
+        title="Tiny Iota"
+        style={{ border: "0", width: "350px", height: "470px" }}
+        src="https://open.spotify.com/embed/album/2iBPZ2dOdW3nAChijHkPq1?utm_source=generator"
+      />
+    ),
+  },
+];
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,20 @@
+import { albumData } from "../data/albumData";
+
 export default function Portfolio() {
   return (
-    <div>
-      <h1>Portfolio</h1>
+    <div className="portfolio-container">
+      <div className="portolio-header">
+        <h1>Portfolio</h1>
+      </div>
+
+      <div className="portfolio-content">
+        {albumData.map((album) => (
+          <div key={album.id}>
+            <h3 className="band-name">Band Name: {album.bandName}</h3>
+            <div className="band-player">{album.player}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
