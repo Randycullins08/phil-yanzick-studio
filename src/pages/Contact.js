aa5fea73d3d93689771a6e572e1df8e0de378885
Randy Cullins <randycullins2018@gmail.com>
2023-01-04 08:48:18 -0700

    started work on contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -11,13 +11,38 @@ export default function Contact() {
         </div>
 
         <div className="form-wrapper">
-          <form action="" method="post">
+          {/* <form onSubmit={handleSubmit} method="post">
             <label htmlFor="name">Name</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
             <label htmlFor="email">Email</label>
-            <input type="email" />
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
             <label htmlFor="message">Message</label>
-            <textarea type="textarea" name="textValue" rows={5} />
+            <textarea
+              type="textarea"
+              name="textValue"
+              rows={5}
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <button type="submit">Send!</button>
+          </form> */}
+          <form method="POST" name="contactform" className="contactForm">
+            <input type="hidden" name="form-name" value="contactForm" />
+            <input type="text" name="name" placeholder="Enter your name" />
+
+            <input type="email" name="email" placeholder="Enter your email" />
+
+            <textarea name="message" placeholder="Messaage"></textarea>
+
+            <button type="submit">Submit</button>
           </form>
         </div>
       </div>
